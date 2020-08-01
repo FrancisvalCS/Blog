fe8c81978cae96a43c89529ee89f7078b707c638
Francisval Cavalcante <francisvalcs@gmail.com>
2020-08-01 18:30:15 -0300

    Projeto Blog ReactJs-Introduzindo a firebase

---
+++ src/firebase.js
@@ -0,0 +1,48 @@
+import app from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyByPTQtZi_XyA_8hbeeLGp3vBftunLFpDo",
+    authDomain: "reactapp-f5547.firebaseapp.com",
+    databaseURL: "https://reactapp-f5547.firebaseio.com",
+    projectId: "reactapp-f5547",
+    storageBucket: "reactapp-f5547.appspot.com",
+    messagingSenderId: "875597826568",
+    appId: "1:875597826568:web:a5d648d51cc57c69e31fbd",
+    measurementId: "G-BZRPTR8FY4"
+  };
+
+
+
+class Firebase{
+  constructor(){
+    // Initialize Firebase
+     app.initializeApp(firebaseConfig)
+     /*if(!firebase.apps.length){
+       firebase.initializeApp(firebaseConfig);
+     }*/
+  }
+
+  login(email, password){
+    return app.auth().signInWithEmailAndPassword(email, password);
+  }
+
+  async register(nome, email, password){
+    await app.auth().createUserWithEmailAndPassword(email, password);
+
+    const uid = app.auth().currentUser.uid;
+
+    return app.database().ref('usuarios').child(uid).set({
+      nome:nome
+    })
+  }
+
+  isInitialized(){
+    return new Promise(resolve => {
+      app.auth().onAuthStateChanged(resolve);
+    })
+  }
+}
+
+export default new Firebase();
